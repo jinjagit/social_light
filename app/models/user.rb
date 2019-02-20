540046cb82af94e99aed03143ece6abc952c67f6
Simon Tharby <jinjamail@gmail.com>
2019-02-20 15:58:56 +0000

    Implement basic login/logout using sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 24 },
             uniqueness: { case_sensitive: false }
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
