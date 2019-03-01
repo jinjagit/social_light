f2bbe308d9f31afeeb0b666111e66b86e3279541
Simon Tharby <jinjamail@gmail.com>
2019-03-01 12:42:03 +0000

    add index to :events, :creator_id

---
+++ db/migrate/20190301123654_add_index_to_events_creator_id.rb
@@ -0,0 +1,5 @@
+class AddIndexToEventsCreatorId < ActiveRecord::Migration[5.2]
+  def change
+    add_index :events, :creator_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_114831) do
+ActiveRecord::Schema.define(version: 2019_03_01_123654) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_02_22_114831) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
